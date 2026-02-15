66d4f129b023ecaf505d3e3c8eec3455965d8237
workonlly <workonlly335@gmail.com>
2026-02-16 02:02:16 +0530

    jfn

--- app/colegesearch.tsx
+++ app/colegesearch.tsx
@@ -40,9 +40,7 @@ const ExploreColleges = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
         </svg>
         <span className='text-sm flex-1'>{college?.college || 'Select Your College'}</span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-        </svg>
+        
       </div>
 
       {/* --- THE SEARCH MODAL --- */}
